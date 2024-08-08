fbb5cb48fd7c549b86e1e1e83000826c606ca20e
Valentin Nazarenko <dxoraxslife@gmail.com>
2024-08-09 01:40:05 +0500

    Upd

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,13 +1,16 @@
-const cacheName = "DefaultCompany-WestRacing-1.01";
+const cacheName = "Game Solutions-WestRacing-1.15";
 const contentToCache = [
-"Build/1CP29JK3BMN0118.loader.js,",
-"Build/1CP29JK3BMN0118.framework.js.unityweb",
-"Build/1CP29JK3BMN0118.data.unityweb",
-"Build/1CP29JK3BMN0118.wasm.unityweb",
+"Build/4Q3PDQSXJYUY8C0.loader.js,",
+"Build/A7GMGA3JCM4YHXP.framework.js.unityweb",
+"Build/A7GMGA3JCM4YHXP.data.unityweb",
+"Build/A7GMGA3JCM4YHXP.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
 
+
+var doCache = true;
+if(!doCache){
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
 
@@ -52,3 +55,30 @@ self.addEventListener("fetch", function (e) {
     })()
   );
 });
+}
+else{
+  self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
+
+}
